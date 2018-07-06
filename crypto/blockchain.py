ee6fa75917983bbbbf5d5adab4f653d451e30bc5
Sharma-Hrishabh <30hrs1999@gmail.com>
2018-07-06 16:36:33 +0000

    Removed error without proof of work

--- crypto/blockchain.py
+++ crypto/blockchain.py
@@ -1,3 +1,4 @@
+# from . import models.py
 from hashlib import sha256
 import time
 import json
@@ -11,16 +12,24 @@ class block:
 		self.sender = sender
 		self.receiver = receiver
 		self.timestamp = timestamp
-		self.hash = ''
+		self.hashe = ''
 		self.nonce = 0
 
 	def calculateHash(self):
 		# returning hash for a set of data
-		return sha256((str(self.index) + str(self.data) + str(self.amount) + str(self.sender) + str(self.receiver) + str(self.timestamp) + str(self.previousHash)).encode())
+		return sha256((str(self.index) + str(self.data) + str(self.amount) + str(self.sender) + str(self.receiver) 
+					+ str(self.timestamp) + str(self.nonce) + str(self.previousHash)).encode())
+	
+	#def mineBlock(self, difficulty):
+	#	codeHash = str(self.hashe)[0:difficulty]
+	#	while not codeHash == "0"*difficulty:
+	#		self.nonce += 1 
+
 
 class blockchain:
 	def __init__(self):
 		self.chain = [self.createGenesis()]
+		self.difficulty = 2
 
 	def createGenesis(self):
 		return block(0 , "Created a genesis Block", time.ctime(), 10 , 'Govenment', 'Govenment', "0")
@@ -29,8 +38,8 @@ class blockchain:
 		return self.chain[-1]
 
 	def addBlock(self, newBlock):
-		newBlock.previousHash = self.getLatestBlock().hash
-		newBlock.hash = newBlock.calculateHash()
+		newBlock.previousHash = self.getLatestBlock().hashe
+		newBlock.hashe = newBlock.calculateHash()
 		self.chain.append(newBlock)
 
 	def isChainValid(self):
@@ -38,10 +47,10 @@ class blockchain:
 			previousNode = self.chain[i-1]
 			currentNode = self.chain[i]
 
-		if currentNode.hash != currentNode.calculateHash():
+		if not currentNode.hashe == currentNode.calculateHash():
 			return False
 
-		if previousNode.hash != currentNode.previousHash:
+		if not previousNode.hashe == currentNode.previousHash:
 			return False
 
 		return True
@@ -49,22 +58,27 @@ class blockchain:
 
 crypto = blockchain()
 
+# mining and printing JSON for block 1
 crypto.addBlock(block(1, "First User added", time.ctime(), 10, "GOVERNMENT", "first_user" ) )
-crypto.addBlock(block(2, "Second User added", time.ctime(), 5, "GOVERNMENT", "second_user") )
-
 lastNode = crypto.getLatestBlock()
 
+print("-------> Block 1 Mined.......")
+
+print(lastNode.hashe.hexdigest())
+
+
+'''
 data = {
 	'index'	: lastNode.index,
 	'data'	: lastNode.data,
 	'previousHash' : lastNode.previousHash.hexdigest(),
-	'currentBlock' : lastNode.hash.hexdigest(),
+	'currentBlock' : lastNode.hashe.hexdigest(),
 	
 	'sender'	:	lastNode.sender,
 	'receiver'	:	lastNode.receiver,
 	'time'		: 	lastNode.timestamp,
 }
 
-print(str(crypto.chain[1].amount) + "\n\n" + str(crypto.chain[2].amount))
+print(json.dumps(data))
+'''
 
-print(json.dumps(data, indent=4))
