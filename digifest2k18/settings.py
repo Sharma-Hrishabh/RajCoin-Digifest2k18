fa1f6bb2dfef298e452735e5b34c8e62aaf06189
Sharma-Hrishabh <30hrs1999@gmail.com>
2018-07-06 14:01:03 +0000

    cloud9

--- digifest2k18/settings.py
+++ digifest2k18/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'crypto',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'digifest2k18.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
